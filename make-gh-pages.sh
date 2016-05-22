cd9381389e8b41fed319477db3ca1a84186d104f
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2016-05-22 07:20:48 +0000

    typo correction and add attribution for GH_PAGES push

--- make-gh-pages.sh
+++ make-gh-pages.sh
@@ -11,10 +11,12 @@ git clone http://github.com/drpaulbrewer/html5csv
 # Delete the code below and substitue *your* deployment code here
 # The code that is below is gh-pages specific
 # and only works on the the main repository
+# This portion inspired by Gergely Nemeth's article on medium.com
+# https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db#.il0d4odzd
 git init 
 git config user.name "Travis-CI"
 git config user.email "travis@eaftc.com"
 git add .
 git commit -m "Autodeploy to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@{$GH_REF}" master:gh-pages >/dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages >/dev/null 2>&1
 
