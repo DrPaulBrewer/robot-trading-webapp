e21050e573d3fb3222e0bfb5e30557ab4d1374eb
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2016-06-09 09:44:41 +0000

    major rewrite, further separate code for visualization vs simulation. create factory methods for quicker visualization development. hack json-editor to convert textarea to positive number array for agg demand/supply. use buyorder and sellorder logs available as of single-market-robot-simulation 0.22.0

--- main.js
+++ main.js
@@ -4,114 +4,301 @@
 /* jshint browserify:true,jquery:true,esnext:true,eqeqeq:true,undef:true,lastsemic:true,strict:true,unused:true */
 /* globals console:true */
 
+require('json-editor'); // defines window.JSONEditor
+
+/* enable use of twitter bootstrap 3 by json editor. requires bootstrap 3 css/js to be loaded in index.html */
+window.JSONEditor.defaults.options.theme = 'bootstrap3';
+window.JSONEditor.defaults.options.iconlib = 'bootstrap3';
+
+/* defines a function accepting string, returning array of positive numbers or undefined */
+const positiveNumberArray = require('positive-number-array');
+
+/* suggested by json-editor README.md lines 1122-1165 */
+window.JSONEditor.defaults.resolvers.unshift(function(schema){
+    'use strict';
+    if (schema.type === "array" && schema.format === "textarea" &&  schema.flavor==="positiveNumber"){
+	return "positiveNumber";
+    }
+});
+
+/* from an examination of json-editor/src/editors/number.js */
+window.JSONEditor.defaults.editors.positiveNumber = window.JSONEditor.defaults.editors.string.extend({
+    sanitize: function(value){
+	'use strict';
+	return (positiveNumberArray(value) || []).join(" ");
+    },
+    getNumColumns: function(){
+	'use strict';
+	return 2;
+    },
+    getValue: function(){
+	'use strict';
+	return positiveNumberArray(this.value) || [];
+    }
+});
+
+const plotly     = require('plotly.js');
+const transpluck = require('transpluck');
+const SMRS       = require('single-market-robot-simulator');
+
+
 var app = (function(){
     'use strict';
-    var SMRS = require('single-market-robot-simulator');
-    require('json-editor'); // defines window.JSONEditor
-    if (!(window.JSONEditor))
-	throw new Error("required json-editor not found at window.JSONEditor");
-    /* enable use of twitter bootstrap 3 by json editor. requires bootstrap 3 css/js to be loaded in index.html */
-    window.JSONEditor.defaults.options.theme = 'bootstrap3';
-    const plotly = require('plotly.js');
-    if (!(plotly))
-	throw new Error("require('plotly.js') failed");
-    function debounce(a, b){
-	var ms = (typeof(a)==='number')? a: b;
-	var func = (typeof(a)==='function')? a: b;
-	var timer;
-	if ((typeof(ms)==='number') && (typeof(func)==='function'))
-	    return function(){
-		clearTimeout(timer);
-		timer = setTimeout(func, ms);
-	    };
-	else 
-	    return function(){};
-    }
-    
-    function asPositiveNumberArray(myInput){
-	if (typeof(myInput)==="string")
-	    return (myInput
-		    .replace(/,/g," ")
-		    .split(/\s+/)
-		    .map(function(s){ return +s; })
-		    .filter(function(n){ return n>0; })
-		   );
-	if (Array.isArray(myInput))
-	    return (myInput
-		    .map(function(s){ return +s; })
-		    .filter(function(n){ return n>0; })
-		   );
-	throw new Error("asPositiveNumberArray: could not parse myInput"+myInput);
-    }
 
     var editor;
+    
+    window.sims=[];
 
-    function init(){
-	var editorElement = document.getElementById('editor');
-	var editorOptions = {
-	    schema: require('./configSchema.json'),
-	    startval: require('./examples-highlow.json')
-	};
-	editor = new window.JSONEditor(editorElement, editorOptions);
+    function yaxisRange(sim){ 
+	return {
+	    yaxis: {
+		range: [(sim.config.L || 0), (sim.config.H || 200)]
+	    }
+	};  
     }
 
-    function main(){
-	$('.paramPlot').html("");
-	$('.resultPlot').html("");
-	editor.getValue().forEach(runSimulation);
+    function plotParameters(sim, slot){
+	var i,l;
+	var xboth=[];
+	var buyerValues = sim.config.buyerValues.slice().sort(function(a,b){ return +b-a;});
+	var sellerCosts = sim.config.sellerCosts.slice().sort(function(a,b){ return +a-b;});
+	for(i=0,l=Math.max(buyerValues.length,sellerCosts.length);i<l;++i){
+	    xboth[i]=1+i;
+	}
+	$('#paramPlot'+slot).html('');
+	var demand = {
+	    name: 'unit value',
+	    x: xboth.slice(0,buyerValues.length),
+	    y: buyerValues,
+	    mode: 'lines+markers',
+	    type: 'scatter'
+	};
+	var supply = {
+	    name: 'unit cost',
+	    x: xboth.slice(0,sellerCosts.length),
+	    y: sellerCosts,
+	    mode: 'lines+markers',
+	    type: 'scatter'
+	};
+	var layout = Object.assign({}, yaxisRange(sim), { xaxis: { range: [0, xboth.length] } } );
+	var plotlyData = [demand, supply];
+	plotly.newPlot('paramPlot'+slot, plotlyData, layout);	
+    }
+
+    function plotFactory(chart){
+	/* chart properties are title, log, names, xs, ys, modes, layout */
+	return function(sim) {
+	    var series = transpluck(sim.logs[chart.log].data, { pluck: [].concat(chart.xs,chart.ys) });
+	    var traces = chart.names.map(function(name,i){
+		return {
+		    name: name,
+		    x: series[chart.xs[i%chart.xs.length]],
+		    y: series[chart.ys[i%chart.ys.length]],
+		    type: 'scatter',
+		    mode: chart.modes[i%chart.modes.length]
+		};
+	    });
+	    var layout = Object.assign({}, {title: chart.title}, yaxisRange(sim), chart.layout);
+	    return [traces, layout];
+	};
+    }
+
+    function histogramFactory(chart){
+	/* chart properties are title, names, logs, vars, bins, range */
+	return function(sim){ 
+	    var traces = chart.names.map(function(name,i){
+		var mylog = chart.logs[i%chart.logs.length];
+		var myvar = chart.vars[i%chart.vars.length];
+		return {
+		    name: name,
+		    x: transpluck(sim.logs[mylog].data, {pluck: [myvar]})[myvar],
+		    type: 'histogram',
+		    opacity:  0.60,
+		    nbinsx: 100
+		};
+	    });
+	    var myrange, mybins;
+	    var mymin,mymax; 
+	    if (chart.range){ 
+		myrange = chart.range;
+	    } else {
+		/* there are only a few traces but datasets can be large */
+		/* avoid stack overflows with large datasets */
+		/* see Linus Unneback's Stack Overflow posting */
+		/* http://stackoverflow.com/a/13440842/103081 */
+		mymin = Math.min.apply(Math, 
+				       traces.map(function(trace){
+					   var x = trace.x;
+					   var j = x.length, min=Infinity;
+					   while(j--)
+					       if (x[j]<min)
+						   min = x[j];
+					   return min;
+				       })
+				      );
+		mymax = Math.max.apply(Math, 
+				       traces.map(function(trace){ 
+					   var x = trace.x;
+					   var j = x.length, max=-Infinity;
+					   while(j--)
+					       if (x[j]>max)
+						   max = x[j];
+					   return max;
+				       })
+				      );
+		myrange = [mymin, mymax];
+	    }
+	    if (chart.bins) { 
+		mybins = chart.bins;
+	    } else {
+		mybins = Math.max(0, Math.min(100, Math.floor(mymax-mymin)));
+	    }
+	    if (mybins && mybins!==100)
+		traces.forEach(function(trace){ trace.nbinsx = mybins; });
+	    var layout ={
+		barmode: 'overlay',
+		xaxis: {
+		    range: myrange
+		},
+		title: chart.title
+	    };
+	    console.log("histogram");
+	    console.log(traces);
+	    console.log(layout);
+	    return [traces, layout];
+	};
+    }
+
+    function prepOHLC(sim){
+	var series = transpluck(sim.logs.ohlc.data,['period','open','high','low','close']);
+	var data = [
+	    {
+		name: 'open',
+		x: series.period,
+		y: series.open,
+		type: 'scatter',
+		mode: 'markers'
+	    },
+	    {
+		name: 'high',
+		x: series.period,
+		y: series.high,
+		type: 'scatter',
+		mode: 'markers'
+	    },
+	    {
+		name: 'low',
+		x: series.period,
+		y: series.low,
+		type: 'scatter',
+		mode: 'markers'
+	    },
+	    {
+		name: 'close',
+		x: series.period,
+		y: series.close,
+		type: 'scatter',
+		mode: 'lines+markers'
+	    }];
+
+	return data;
+    }
+
+
+/* do not need this next function yet ---
+
+    function plotOHLCHistogram(sim, slot){
+	var layout = {
+	    barmode: 'overlay',
+	    xaxis: {
+		range: [
+		    (sim.config.L || 0),
+		    (sim.config.buyerValues[0] || 100)
+		]
+	    }
+	};
+	var data = (prepOHLC(sim)
+		    .filter(function(plot){
+			return (plot.name==='open') || (plot.name==='close');
+		    })
+		    .map(function(plot){
+			plot.x = plot.y;
+			delete plot.y;
+			delete plot.mode;
+			plot.type = 'histogram';
+			plot.opacity = 0.60;
+			plot.nbinsx = layout.xaxis.range[1]-layout.xaxis.range[0];
+			return plot;
+		    })
+		   );
+	
+	plotly.newPlot('resultPlot'+slot, data, layout);
+    }
+
+*/
+
+    function plotOHLCTimeSeries(sim){
+	var layout = yaxisRange(sim);	
+	var data = prepOHLC(sim);
+
+	return [data, layout];
+
+    }
+
+    var visual = 0;
+	
+    var smallDataVisuals = [
+	plotOHLCTimeSeries,
+	plotFactory({
+	    title: "trade volume by period",
+	    log: "volume",
+	    names: ["volume"],
+	    xs: ['period'],
+	    ys: ['volume'],
+	    modes: ['markers'],
+	    layout: {yaxis: { }}
+	}),
+	plotFactory({
+	    title: "trade prices",
+	    log: "trade",
+	    names: ["trades"],
+	    xs: ['t'],
+	    ys: ['price'],
+	    modes: ['markers+lines']
+	}),
+	plotFactory({
+	    title: "trade profitability",
+	    log: "trade",
+	    names: ["trade","unit value","unit cost"],
+	    xs: ["t"],
+	    ys: ['price','buyerValue','sellerCost'],
+	    modes: ["markers+lines","markers","markers"]
+	}),
+	histogramFactory({
+	    title: "order price distribution",
+	    names: ["buyLimitPrice","sellLimitPrice"],
+	    logs: ["buyorder","sellorder"],
+	    vars: ["buyLimitPrice","sellLimitPrice"]
+	})
+    ];
+
+    function showSimulation(simConfig, slot){
+	var plotParams = smallDataVisuals[visual](simConfig);
+	plotParams.unshift('resultPlot'+slot);
+	plotly.newPlot.apply(plotly, plotParams);
     }
 
     function runSimulation(simConfig, slot){
-	// clear any leftovers
-	$('#paramPlot'+slot).html("");
-	$('#resultPlot'+slot).html("");
 	// set up and run new simulation
+	// make a copy to prevent side effects
 	var config = Object.assign({}, simConfig);
 	config.xMarket = Object.assign({}, simConfig.xMarket);
+	// set booklimit, bookfixed
 	var booklimit = Math.max(config.buySellBookLimit, config.buyerImprovementRuleLevel, config.sellerImprovementRuleLevel);
 	config.bookfixed = 1;
 	config.booklimit = booklimit || 10;
 
-	var redrawStepChart = function(sim, slot){
-	    var i,l;
-	    var xboth=[];
-	    var buyerValues = asPositiveNumberArray(sim.options.buyerValues);
-	    var sellerCosts = asPositiveNumberArray(sim.options.sellerCosts);
-	    for(i=0,l=Math.max(buyerValues.length,sellerCosts.length);i<l;++i){
-		xboth[i]=1+i;
-	    }
-	    $('#paramPlot'+slot).html('');
-	    var demand = {
-		name: 'unit value',
-		x: xboth.slice(0,buyerValues.length),
-		y: buyerValues,
-		mode: 'lines+markers',
-		type: 'scatter'
-	    };
-	    var supply = {
-		name: 'unit cost',
-		x: xboth.slice(0,sellerCosts.length),
-		y: sellerCosts,
-		mode: 'lines+markers',
-		type: 'scatter'
-	    };
-	    var layout = {
-		xaxis: {
-		    range: [0, xboth.length]
-		},
-		yaxis: {
-		    range: [0,200]
-		}
-	    };
-	    var plotlyData = [demand, supply];
-	    plotly.newPlot('paramPlot'+slot, plotlyData, layout);
-	    
-	};
-
-
-	
 	var onPeriod = function(e,sim){
-	    console.log(sim.period, config.periods, slot);
 	    if (sim.period<config.periods){
 		$('#resultPlot'+slot).html("<h1>"+Math.round(100*sim.period/config.periods)+"% complete</h1>");
 	    } else {
@@ -119,151 +306,42 @@ var app = (function(){
 	    }
 	};
 
-	var asSeries = function(data){
-	    var result = {};
-	    var header = data[0];
-	    var i,l,row,j,k;
-	    for(i=0,l=header.length;i<l;++i)
-		result[header[i]] = [];
-	    for(i=1,l=data.length;i<l;++i){
-		row = data[i];
-		for(j=0,k=row.length;j<k;++j)
-		    result[header[j]][i-1] = row[j];
-	    }
-	    return result;
-	};
-
-	var plotPriceTimeSeries = function(sim){
-	    var tCol = sim.logs.trade.data[0].indexOf("t");
-	    var priceCol = sim.logs.trade.data[0].indexOf("price");
-	    var i,l,x=[],y=[];
-	    var d = sim.logs.trade.data;
-	    for(i=1,l=d.length;i<l;++i){
-		x[i-1] = d[i][tCol];
-		y[i-1] = d[i][priceCol];
-	    }
-	    var trace = {
-		x: x,
-		y: y,
-		type: 'scatter',
-		mode: 'lines+markers'
-	    };
-	    var layout = {
-		yaxis: {
-		    range: [0,200]
-		}
-	    };
-	    plotly.newPlot('resultPlot'+slot, [trace], layout);
-	};
-
-	var prepOHLC = function(sim){
-	    if (sim.logs.ohlc.data[0][0]!=='period')
-		sim.logs.ohlc.data.unshift(['period',
-					    'open',
-					    'high',
-					    'low',
-					    'close']);
-	    var series = asSeries(sim.logs.ohlc.data);
-	    var data = [
-		{
-		    name: 'open',
-		    x: series.period,
-		    y: series.open,
-		    type: 'scatter',
-		    mode: 'markers'
-		},
-		{
-		    name: 'high',
-		    x: series.period,
-		    y: series.high,
-		    type: 'scatter',
-		    mode: 'markers'
-		},
-		{
-		    name: 'low',
-		    x: series.period,
-		    y: series.low,
-		    type: 'scatter',
-		    mode: 'markers'
-		},
-		{
-		    name: 'close',
-		    x: series.period,
-		    y: series.close,
-		    type: 'scatter',
-		    mode: 'lines+markers'
-		}];
-
-	    return data;
-	};
-
-	var plotOHLC = function(sim){
-	    var layout = {
-		yaxis: {
-		    range: [0,200]
-		}
-	    };
-	    
-	    var data = prepOHLC(sim);
-
-	    plotly.newPlot('resultPlot'+slot, data, layout);
-
-	};
-
-	var plotOHLCHistogram = function(sim){
-	    var layout = {
-		barmode: 'overlay',
-		xaxis: {
-		    range: [0,100]
-		}
-	    };
-	    var data = (prepOHLC(sim)
-			.filter(function(plot){
-			    return (plot.name==='open') || (plot.name==='close');
-			})
-			.map(function(plot){
-			    plot.x = plot.y;
-			    delete plot.y;
-			    delete plot.mode;
-			    plot.type = 'histogram';
-			    plot.opacity = 0.60;
-			    plot.nbinsx = 100;
-			    return plot;
-			})
-		       );
-	    
-	    plotly.newPlot('resultPlot'+slot, data, layout);
-	};
-
-	var visualizations = [plotOHLCHistogram, plotOHLC, plotPriceTimeSeries];
-
-	var clickCounter = 0;
-
-	var draw = function(sim){
-	    var drawme = function(){ draw(sim); };
-	    $('#changeVisualization').off('click', drawme);
-	    $('#resultPlot'+slot).html("");
-	    visualizations[clickCounter](sim);
-	    clickCounter = (1+clickCounter)%(visualizations.length);
-	    setTimeout(function(){
-		$('#changeVisualization').on('click',  drawme);
-	    }, 500);
-	};
-
 	var onDone = function(e,sim){
-	    draw(sim);
-	    setTimeout(redrawStepChart, 500, sim);
+	    showSimulation(sim, slot);
 	}; 
 
-	var mysim = SMRS.runSimulation(config, onDone, onPeriod); 
-	if (config.periods>=100)
-	    delete mysim.logs.order;
-	redrawStepChart(mysim, slot);
+	var mysim = SMRS.runSimulation(simConfig, onDone, onPeriod); 
+	plotParameters(mysim, slot);
+
+	return mysim;
+	
     }
+
+    /* expose these app functions to outside code */
+
     return {
-	init: init,
-	main: main
+	init: function init(){
+	    var editorElement = document.getElementById('editor');
+	    var editorOptions = {
+		schema: require('./configSchema.json'),
+		startval: require('./examples-highlow.json')
+	    };
+	    editor = new window.JSONEditor(editorElement, editorOptions);
+	},
+
+	run: function run(){
+	    $('.paramPlot').html("");
+	    $('.resultPlot').html("");
+	    var config = editor.getValue();
+	    sims = config.map(runSimulation);
+	},
+	
+	next: function next(){ 
+	    visual = (1+visual) % smallDataVisuals.length;
+	    sims.forEach(showSimulation);
+	}
     };
+
 })();
 
 $(function(){
@@ -271,5 +349,5 @@ $(function(){
     app.init();
 });
 
-$('#runButton').click(app.main);
-
+$('#runButton').click(app.run);
+$('#nextButton').click(app.next);
